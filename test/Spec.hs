a9eb84cd2c8c3d700b0edf69e60c702d77e60b0a
Ruly Rudel <ruly.rudel@gmail.com>
2023-11-26 13:24:07 +0900

    add testbench.

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,12 +1,96 @@
 {-# LANGUAGE BinaryLiterals #-}
 {-# LANGUAGE OverloadedStrings #-}
 
-import qualified Data.Text as T
+import Test.HUnit
+import Control.Monad ( void )
 import CaseBuilder (Def(..), Sel(..), buildCase)
-import qualified Data.Text.IO as T
 
 main :: IO ()
-main = let x = Case "op" 2
+main = do
+  void $ runTestTT $ TestList
+    [
+      "00_00" ~: ["rd = imm_u;\n"] ~=? buildCase 0 (RTL "rd = imm_u;"),
+
+      "00_01" ~: [
+        "case(op)\n",
+        "  2'd3: // [32bit instruction]\n",
+        "  default: id_we = 1'b0;\n",
+        "endcase\n"
+        ] ~=?
+        buildCase 0 (Case "op" 2 [Sel 0b11 "[32bit instruction]" []]),
+
+      "00_02" ~: [
+        "case(op)\n",
+        "  2'd3: // [32bit instruction]\n",
+        "    case(opcode)\n",
+        "      5'd24: // [BRANCH]\n",
+        "        case(funct3)\n",
+        "          3'd0: // [BEQ]\n",
+        "            next_pc = RS1 == RS2 ? PC + imm_b : PC + 'h4;\n",
+        "          3'd1: // [BNE]\n",
+        "            next_pc = RS1 != RS2 ? PC + imm_b : PC + 'h4;\n",
+        "          default: id_we = 1'b0;\n",
+        "        endcase\n","      5'd5: // [AUIPC]\n",
+        "        rd = PC + imm_u;\n",
+        "      5'd13: // [LUI]\n",
+        "        rd = imm_u;\n",
+        "      5'd6: // [OP-IMM-32]\n",
+        "        case(funct3)\n",
+        "          3'd0: // [ADDIW]\n",
+        "            rd = s32to64(RS1[31:0] + imm_i[31:0]);\n",
+        "          3'd1: // \n",
+        "            case(funct7)\n",
+        "              7'd0: // [SLLIW]\n",
+        "                rd = s32to64(RS1[31:0] << shamt[4:0]);\n",
+        "              default: id_we = 1'b0;\n",
+        "            endcase\n",
+        "          3'd5: // \n",
+        "            case(funct7)\n",
+        "              7'd0: // [SRLIW]\n",
+        "                rd = s32to64(RS1[31:0] >> shamt[4:0]);\n",
+        "              7'd32: // [SRAIW]\n",
+        "                rd = s32to64($signed(RS1[31:0]) >>> shamt[4:0]);\n",
+        "              default: id_we = 1'b0;\n",
+        "            endcase\n",
+        "          default: id_we = 1'b0;\n",
+        "        endcase\n",
+        "      default: id_we = 1'b0;\n",
+        "    endcase\n",
+        "  default: id_we = 1'b0;\n",
+        "endcase\n"
+      ] ~=?
+        buildCase 0 (
+          Case "op" 2
+                [Sel 0b11 "[32bit instruction]"
+                  [Case "opcode" 5
+                    [Sel 0b11000 "[BRANCH]"
+                      [Case "funct3" 3
+                        [Sel 0b000 "[BEQ]"
+                          [RTL "next_pc = RS1 == RS2 ? PC + imm_b : PC + 'h4;"],
+                         Sel 0b001 "[BNE]"
+                          [RTL "next_pc = RS1 != RS2 ? PC + imm_b : PC + 'h4;"]]],
+                     Sel 0b00101 "[AUIPC]"
+                        [RTL "rd = PC + imm_u;"],
+                     Sel 0b01101 "[LUI]"
+                        [RTL "rd = imm_u;"],
+                     Sel 0b00110 "[OP-IMM-32]"
+                        [Case "funct3" 3
+                          [Sel 0b000 "[ADDIW]"
+                             [RTL "rd = s32to64(RS1[31:0] + imm_i[31:0]);"],
+                           Sel 0b001 ""
+                             [Case "funct7" 7
+                               [Sel 0b0000000 "[SLLIW]"
+                                  [RTL "rd = s32to64(RS1[31:0] << shamt[4:0]);"]]],
+                           Sel 0b101 ""
+                             [Case "funct7" 7
+                               [Sel 0b0000000 "[SRLIW]"
+                                  [RTL "rd = s32to64(RS1[31:0] >> shamt[4:0]);"],
+                                Sel 0b0100000 "[SRAIW]"
+                                  [RTL "rd = s32to64($signed(RS1[31:0]) >>> shamt[4:0]);"]]]]]]]])
+    ]
+
+{-
+  let x = Case "op" 2
                 [Sel 0b11 "[32bit instruction]" 
                   [Case "opcode" 5 
                     [Sel 0b11000 "[BRANCH]"
@@ -35,3 +119,4 @@ main = let x = Case "op" 2
                                   [RTL "rd = s32to64($signed(RS1[31:0]) >>> shamt[4:0]);"]]]]]]]] in
     do
         putStrLn $ concat $ buildCase 0 x
+-}
