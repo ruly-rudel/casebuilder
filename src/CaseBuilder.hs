7c72ac025f380b62e1b855828ca30904b82d08f1
Ruly Rudel <ruly.rudel@gmail.com>
2023-11-27 14:10:32 +0900

    using ByteString.

--- src/CaseBuilder.hs
+++ src/CaseBuilder.hs
@@ -3,31 +3,33 @@
 
 module CaseBuilder ( Def(..), Sel(..), Expr(..), buildCase, parseCDF ) where
 
-data Sel = Sel  Int String [Def]
-data Def = Case String Int [Sel] |
-           RTL  String
+import qualified Data.ByteString.Char8 as BS
 
-indent :: Int -> String -> String
-indent n x = replicate n ' ' ++ x
+data Sel = Sel  Int BS.ByteString [Def]
+data Def = Case BS.ByteString Int [Sel] |
+           RTL  BS.ByteString
 
-buildCase :: Int -> Def -> [String]
+indent :: Int -> BS.ByteString -> BS.ByteString
+indent n x = BS.replicate n ' ' `mappend` x
+
+buildCase :: Int -> Def -> [BS.ByteString]
 buildCase n x = case x of
     Case switch bits sels ->
-        (indent n ("case(" ++ switch ++ ")\n") : concatMap (buildSel bits (n + 2)) sels)
+        (indent n ("case(" `mappend` switch `mappend` ")\n") : concatMap (buildSel bits (n + 2)) sels)
          ++ [indent (n + 2) "default: id_we = 1'b0;\n", indent n "endcase\n"]
-    RTL text -> [indent n (text ++ "\n")]
+    RTL text -> [indent n (text `mappend` "\n")]
 
-buildSel :: Int -> Int -> Sel -> [String]
+buildSel :: Int -> Int -> Sel -> [BS.ByteString]
 buildSel bits n x = case x of
     Sel val comment defs ->
-        indent n (show bits ++ "'d" ++ show val ++ ": // " ++ comment ++ "\n") :
-        concatMap (buildCase (n + 2)) defs
+        BS.concat [indent n $ BS.pack (show bits), "'d", BS.pack (show val), ": // ", comment, "\n"] : 
+            concatMap (buildCase (n + 2)) defs
 
-data Expr = CaseSel Int String (Int, Int) String [Expr] |
-            Code Int String
+data Expr = CaseSel Int BS.ByteString (Int, Int) BS.ByteString [Expr] |
+            Code Int BS.ByteString
             deriving (Show, Eq)
 
 parseCDF :: String -> Expr
 parseCDF x =
     CaseSel 0 "op" (2, 0b11) "[32bit instruction]" []
-            
+
